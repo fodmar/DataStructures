b58a6beae1849bc6f05b11d93f7e4f35d95656ed
fodmar <marcinfoder@gmail.com>
2017-06-21 21:15:53 +0200

    add tests to remove

--- DataStructures.Tests/Scenarios/Collection/RemoveScenarioGenerator.cs
+++ DataStructures.Tests/Scenarios/Collection/RemoveScenarioGenerator.cs
@@ -69,6 +69,70 @@ namespace DataStructures.Tests.Scenarios.Collection
                 ExpectedElements = Generate("0 1 2 3 4"),
                 ExpectedRemoved = false
             };
+
+            yield return new RemoveScenario
+            {
+                Initial = Generate("55"),
+                ToRemove = GenerateOne(55),
+                ExpectedElements = new TestItem[0],
+                ExpectedRemoved = true
+            };
+
+            yield return new RemoveScenario
+            {
+                Initial = Generate("55 33"),
+                ToRemove = GenerateOne(55),
+                ExpectedElements = Generate("33"),
+                ExpectedRemoved = true
+            };
+
+            yield return new RemoveScenario
+            {
+                Initial = Generate("55 33"),
+                ToRemove = GenerateOne(33),
+                ExpectedElements = Generate("55"),
+                ExpectedRemoved = true
+            };
+
+            yield return new RemoveScenario
+            {
+                Initial = Generate("55 77"),
+                ToRemove = GenerateOne(55),
+                ExpectedElements = Generate("77"),
+                ExpectedRemoved = true
+            };
+
+            yield return new RemoveScenario
+            {
+                Initial = Generate("55 77 33"),
+                ToRemove = GenerateOne(55),
+                ExpectedElements = Generate("77 33"),
+                ExpectedRemoved = true
+            };
+
+            yield return new RemoveScenario
+            {
+                Initial = Generate("4 3 1 2 5 6 7"),
+                ToRemove = GenerateOne(5),
+                ExpectedElements = Generate("4 3 1 2 6 7"),
+                ExpectedRemoved = true
+            };
+
+            yield return new RemoveScenario
+            {
+                Initial = Generate("4 3 1 2 5 6 7"),
+                ToRemove = GenerateOne(4),
+                ExpectedElements = Generate("5 3 1 2 6 7"),
+                ExpectedRemoved = true
+            };
+
+            yield return new RemoveScenario
+            {
+                Initial = Generate("4 3 1 2 5 6 7"),
+                ToRemove = GenerateOne(10),
+                ExpectedElements = Generate("4 3 1 2 5 6 7"),
+                ExpectedRemoved = false
+            };
         }
     }
 }
